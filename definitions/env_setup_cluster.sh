51289cfded770b0fd60e98a6062c395d3aabc68a
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2022-10-28 15:38:08 +0100

    Disable Feast telemetry

--- definitions/env_setup_cluster.sh
+++ definitions/env_setup_cluster.sh
@@ -2,3 +2,4 @@
 export FEAST_S3_ENDPOINT_URL="http://minio-service.kubeflow.svc.cluster.local:9000"
 export AWS_ACCESS_KEY_ID="minio"
 export AWS_SECRET_ACCESS_KEY="minio123"
+export FEAST_USAGE=False
