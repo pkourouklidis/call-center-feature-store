4a0aef0e1bd5616dfd5164d30f3719b24cdc839a
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2022-10-28 14:04:31 +0100

    Supply entity dataframe via sql query

--- scripts/createNamedDataset.py
+++ scripts/createNamedDataset.py
@@ -1,5 +1,6 @@
 from feast import FeatureStore
 from feast.infra.offline_stores.file_source import SavedDatasetFileStorage
+from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source import SavedDatasetPostgreSQLStorage
 import pandas as pd
 import os
 
@@ -10,20 +11,18 @@ os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
 os.environ["AWS_ACCESS_KEY_ID"] = "minio"
 os.environ["FEAST_S3_ENDPOINT_URL"] = "http://minio-service.kubeflow.svc.cluster.local:9000"
 
-store = FeatureStore(repo_path=".")
+store = FeatureStore(repo_path="../definitions")
 
 # Get the latest feature values for unique entities
-entity_df = pd.DataFrame.from_dict({"callID": [0, 1, 2, 3, 4],})
-entity_df["event_timestamp"] = pd.to_datetime("now", utc=True)
 historicalJob = store.get_historical_features(
-    entity_df=entity_df, features=["callFeatures:waitDuration"],
+    entity_df="select id, arrival_time as event_timestamp from call_data limit 100",
+     features=["callcenter:waitduration"],
 )
 
 dataset = store.create_saved_dataset(
     from_=historicalJob,
     name='callcenter_training',
-    storage=SavedDatasetFileStorage(path='s3://feast/data/callcenter_training.parquet',
-    s3_endpoint_override='http://minio-service.kubeflow.svc.cluster.local:9000')
+    storage=SavedDatasetPostgreSQLStorage("test2")
 )
 
 print(dataset.to_df())
