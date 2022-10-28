645bfbc689dd51be282fe2d8de2f4b66c5318fdf
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2022-10-28 15:29:47 +0100

    Use closing_time as the feature timestamp

--- definitions/dataSources.py
+++ definitions/dataSources.py
@@ -2,7 +2,7 @@ from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source i
 
 callSource = PostgreSQLSource(
     name="callSource",
-    query="select id, arrival_time as timestamp, round(extract(seconds from pickup_time-arrival_time)) as waitDuration, round(extract(seconds from closing_time-pickup_time)) as serviceDuration from call_data",
+    query="select id, closing_time as timestamp, round(extract(seconds from pickup_time-arrival_time)) as waitDuration, round(extract(seconds from closing_time-pickup_time)) as serviceDuration from call_data",
     timestamp_field="timestamp",
     description="callcenter logs"
 )
