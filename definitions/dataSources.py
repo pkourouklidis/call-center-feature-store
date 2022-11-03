d1b06a306297d43486141d38230b0d1ac99a9e2a
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2022-11-03 16:04:46 +0000

    add customer happiness in the feature store

--- definitions/dataSources.py
+++ definitions/dataSources.py
@@ -2,7 +2,7 @@ from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source i
 
 callSource = PostgreSQLSource(
     name="callSource",
-    query="select id, closing_time as timestamp, round(extract(seconds from pickup_time-arrival_time)) as waitDuration, round(extract(seconds from closing_time-pickup_time)) as serviceDuration from call_data",
+    query="select call_data.id as id, closing_time as timestamp, round(extract(seconds from pickup_time-arrival_time)) as wait_duration, round(extract(seconds from closing_time-pickup_time)) as service_duration, is_happy from call_data inner join customer on customer.id=call_data.customer_id",
     timestamp_field="timestamp",
     description="callcenter logs"
 )
