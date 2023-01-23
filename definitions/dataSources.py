d4771e2c1713bb18c552e7b27fe0cde3b9d06942
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-01-23 17:17:45 +0000

    add hapiness prediction to the feature store

--- definitions/dataSources.py
+++ definitions/dataSources.py
@@ -4,7 +4,7 @@ from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source i
 
 callSource = PostgreSQLSource(
     name="callSource",
-    query="select call_data.id as id, closing_time as timestamp, round(extract(seconds from pickup_time-arrival_time)) as wait_duration, round(extract(seconds from closing_time-pickup_time)) as service_duration, is_solved::int, is_happy::int from call_data inner join customer on customer.id=call_data.customer_id",
+    query="select call_data.id as id, closing_time as timestamp, round(extract(seconds from pickup_time-arrival_time)) as wait_duration, round(extract(seconds from closing_time-pickup_time)) as service_duration, is_solved::int, is_happy::int, is_predicted_to_be_happy::int as hapiness_prediction from call_data inner join customer on customer.id=call_data.customer_id",
     timestamp_field="timestamp",
     description="callcenter logs",
 )
