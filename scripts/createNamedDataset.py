56378a71a9cb47c234a46cfa0708f4121449fb51
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-02-06 17:39:21 +0000

    add new datasource

--- scripts/createNamedDataset.py
+++ scripts/createNamedDataset.py
@@ -1,6 +1,8 @@
 from feast import FeatureStore
 from feast.infra.offline_stores.file_source import SavedDatasetFileStorage
-from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source import SavedDatasetPostgreSQLStorage
+from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source import (
+    SavedDatasetPostgreSQLStorage,
+)
 import pandas as pd
 import os
 
@@ -11,13 +13,19 @@ store = FeatureStore(repo_path="../definitions")
 # Get the latest feature values for unique entities
 historicalJob = store.get_historical_features(
     entity_df="select id, closing_time as event_timestamp from call_data order by event_timestamp desc limit 100",
-     features=["callcenter:wait_duration", "callcenter:service_duration", "callcenter:is_solved"],
+    features=[
+        "callcenter:wait_duration",
+        "callcenter:service_duration",
+        "callcenter:is_solved",
+        "callcenter:is_happy",
+        "callcenter:happiness_prediction",
+    ],
 )
 
 dataset = store.create_saved_dataset(
     from_=historicalJob,
-    name='callcenter-linear_training',
-    storage=SavedDatasetPostgreSQLStorage("test")
+    name="callcenter-tree_training",
+    storage=SavedDatasetPostgreSQLStorage("test"),
 )
 
 print(dataset.to_df())
