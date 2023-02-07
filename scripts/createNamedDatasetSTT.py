61bf9ceb9d8d7f6c31389752124dcd0b92dd9460
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-02-07 15:40:46 +0000

    register postgres adapter to handle numpy arrays

--- scripts/createNamedDatasetSTT.py
+++ scripts/createNamedDatasetSTT.py
@@ -4,7 +4,16 @@ from feast import FeatureStore
 from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source import (
     SavedDatasetPostgreSQLStorage,
 )
+import numpy as np
+from psycopg2.extensions import register_adapter, AsIs
 
+
+def addapt_numpy_array(numpy_array):
+    stringRepresentation = ",".join(str(element) for element in numpy_array)
+    return AsIs("'{" + stringRepresentation + "}'")
+
+
+register_adapter(np.ndarray, addapt_numpy_array)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 store = FeatureStore(repo_path="../definitions")
